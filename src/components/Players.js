f2da21227512edacb4e914de62dfc165657438e1
dhp131 <daophuong131.work@gmail.com>
2024-01-12 00:40:44 +0700

    Components part 2

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,75 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
+import { Players } from "../shared/ListOfPlayers";
 
-export default class player extends Component {
-	render() {
-		return (
-			<div className="container">
+export default function PlayersComponent() {
+	return (
+		<div className="container">
+			{Players.map((player) => (
 				<div className="column">
 					<div className="card">
-						<img src="assets/images/cr.jpg" />
-						<h3>Cristiano Ronaldo</h3>
-						<p className="title">Manchester United</p>
+						<img src={player.img} alt={player.name} />
+						<h3>{player.name}</h3>
+						<p className="title">{player.club}</p>
 						<p>
 							<button>Detail</button>
 						</p>
 					</div>
 				</div>
-
-				<div className="column">
-					<div className="card">
-						<img src="assets/images/kante.jpg" alt="" />
-						<h3>Kante</h3>
-						<p className="title">Chelsea</p>
-						<p>
-							<button>Detail</button>
-						</p>
-					</div>
-				</div>
-
-				<div className="column">
-					<div className="card">
-						<img src="assets/images/messi.jpg" alt="" />
-						<h3>Messi</h3>
-						<p className="title">PSG</p>
-						<p>
-							<button>Detail</button>
-						</p>
-					</div>
-				</div>
-
-				<div className="column">
-					<div className="card">
-						<img src="assets/images/neymar.jpg" alt="" />
-						<h3>Neymar</h3>
-						<p className="title">PSG</p>
-						<p>
-							<button>Detail</button>
-						</p>
-					</div>
-				</div>
-
-				<div className="column">
-					<div className="card">
-						<img src="assets/images/kane.jpg" alt="" />
-						<h3>Kane</h3>
-						<p className="title">Tottemham</p>
-						<p>
-							<button>Detail</button>
-						</p>
-					</div>
-				</div>
-
-				<div className="column">
-					<div className="card">
-						<img src="assets/images/haaland.jpg" alt="" />
-						<h3>Haaland</h3>
-						<p className="title">Manchester City</p>
-						<p>
-							<button>Detail</button>
-						</p>
-					</div>
-				</div>
-			</div>
-		);
-	}
+			))}
+		</div>
+	);
 }
