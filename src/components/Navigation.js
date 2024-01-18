bd97b2f66d27a59f96a19c362c6da66651568c11
dhp131 <daophuong131.work@gmail.com>
2024-01-18 23:19:05 +0700

    useEffect/useContext

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,74 @@
 import React from "react";
-
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
 export default function Navigation() {
+	const { theme, toggle, dark } = useContext(ThemeContext);
 	return (
 		<div>
-			<nav>
-				<ul className="navbar">
+			<nav
+				style={{
+					backgroundColor: theme.backgroundColor,
+					color: theme.color,
+				}}
+			>
+				<ul>
 					<li>
 						<a className="active" href="#home">
 							Home
 						</a>
 					</li>
 					<li>
-						<a href="#news">News</a>
+						<a
+							style={{
+								backgroundColor: theme.backgroundColor,
+								color: theme.color,
+							}}
+							className="active1"
+							href="#news"
+						>
+							News
+						</a>
 					</li>
 					<li>
-						<a href="#about">About</a>
+						<a
+							style={{
+								backgroundColor: theme.backgroundColor,
+								color: theme.color,
+							}}
+							className="active1"
+							href="#about"
+						>
+							About
+						</a>
 					</li>
 					<li>
-						<a href="#contact">Contact</a>
+						<a
+							style={{
+								backgroundColor: theme.backgroundColor,
+								color: theme.color,
+							}}
+							className="active1"
+							href="#contact"
+						>
+							Contact
+						</a>
 					</li>
 				</ul>
+				<div className="switch" style={{ position: "relative" }}>
+					<a
+						className="switch-mode"
+						href="#"
+						onClick={toggle}
+						style={{
+							backgroundColor: theme.backgroundColor,
+							color: theme.color,
+							outline: "none",
+						}}
+						data-testid="toggle-theme-btn"
+					>
+						Switch Nav to {!dark ? "Dark" : "Light"} mode
+					</a>
+				</div>
 			</nav>
 		</div>
 	);
